e4c2256be228500f5613c91af2fcf2f28d5e6d32
Mardeson Pereira <maxpb777@gmail.com>
2025-07-21 21:50:13 -0300

    fix: cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "monitor-replaymes.us2.pitunnel.com",
+    "192.168.23.169:3000",
+  ],
 };
 
 export default nextConfig;
